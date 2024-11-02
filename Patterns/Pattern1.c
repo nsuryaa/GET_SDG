924356959f6f0a147e25e8b256a2ef115324b5c9
nsuryaa <suryasedulous@gmail.com>
2024-11-02 12:01:41 +0530

    P2

--- Patterns/Pattern1.c
+++ Patterns/Pattern1.c
@@ -5,6 +5,8 @@ int main()
 {
     int iNumber = 5, iIterator1 = 0, iIterator2 = 0;
 
+    /*Digit*/
+    /*
     for (iIterator1 = 1; iIterator1 < (iNumber * 2); iIterator1++)
     {
         for (iIterator2 = 1; iIterator2 < (iNumber * 2); iIterator2++)
@@ -29,6 +31,45 @@ int main()
                 if ((iIterator2 >= (iIterator1 - iNumber + 1)) && (iIterator2 <= ((iNumber + (iNumber - 2)) - (iIterator1 - iNumber - 1))))
                 {
                     printf("%d", iIterator2);
+                }
+                else
+                {
+                    printf(" ");
+                }
+            }
+            else
+            {
+                printf(" ");
+            }
+        }
+        printf("\n");
+    }
+    */
+
+    for (iIterator1 = 1; iIterator1 < (iNumber * 2); iIterator1++)
+    {
+        for (iIterator2 = 1; iIterator2 < (iNumber * 2); iIterator2++)
+        {
+            if ((iIterator1 == iNumber) || ((iIterator1 == 1) && (iIterator2 == iNumber)) || ((iIterator1 == ((iNumber * 2) - 1)) && (iIterator2 == iNumber)))
+            {
+                printf("*");
+            }
+            else if ((iIterator1 >= 2) && (iIterator1 <= (iNumber - 1)))
+            {
+                if ((iIterator2 >= (iNumber + 1 - iIterator1)) && (iIterator2 <= (iNumber - 1 + iIterator1)))
+                {
+                    printf("*");
+                }
+                else
+                {
+                    printf(" ");
+                }
+            }
+            else if ((iIterator1 > iNumber) && (iIterator1 < ((iNumber * 2) - 1)))
+            {
+                if ((iIterator2 >= (iIterator1 - iNumber + 1)) && (iIterator2 <= ((iNumber + (iNumber - 2)) - (iIterator1 - iNumber - 1))))
+                {
+                    printf("*");
                 }
                 else
                 {
---
+++ Patterns/Pattern2.c
@@ -0,0 +1,86 @@
+#include <stdio.h>
+#include <curses.h>
+
+int main()
+{
+    int iNumber = 5, iIterator1 = 0, iIterator2 = 0;
+
+    /*Digit*/
+    /*
+    for (iIterator1 = 1; iIterator1 < (iNumber * 2); iIterator1++)
+    {
+        for (iIterator2 = 1; iIterator2 < (iNumber * 2); iIterator2++)
+        {
+            if (((iIterator1 == 1) && (iIterator2 == iNumber)) || ((iIterator1 == ((iNumber * 2) - 1)) && (iIterator2 == iNumber)))
+            {
+                printf("%d", iIterator2);
+            }
+            else if (iIterator1 >= 2 && iIterator1 <= iNumber)
+            {
+                if ((iIterator2 == (iNumber - iIterator1 + 1)) || (iIterator2 == (iNumber + iIterator1 - 1)))
+                {
+                    printf("%d", iIterator2);
+                }
+                else
+                {
+                    printf(" ");
+                }
+            }
+            else if ((iIterator1 > iNumber) && (iIterator1 < ((iNumber * 2) - 1)))
+            {
+                if (((iIterator2 == (iIterator1 - iNumber + 1))) || ((iIterator2 == (iNumber * 2) - (iIterator1 - iNumber + 1))))
+                {
+                    printf("%d", iIterator2);
+                }
+                else
+                {
+                    printf(" ");
+                }
+            }
+            else
+            {
+                printf(" ");
+            }
+        }
+        printf("\n");
+    }
+    */
+
+    for (iIterator1 = 1; iIterator1 < (iNumber * 2); iIterator1++)
+    {
+        for (iIterator2 = 1; iIterator2 < (iNumber * 2); iIterator2++)
+        {
+            if (((iIterator1 == 1) && (iIterator2 == iNumber)) || ((iIterator1 == ((iNumber * 2) - 1)) && (iIterator2 == iNumber)))
+            {
+                printf("*");
+            }
+            else if (iIterator1 >= 2 && iIterator1 <= iNumber)
+            {
+                if ((iIterator2 == (iNumber - iIterator1 + 1)) || (iIterator2 == (iNumber + iIterator1 - 1)))
+                {
+                    printf("*");
+                }
+                else
+                {
+                    printf(" ");
+                }
+            }
+            else if ((iIterator1 > iNumber) && (iIterator1 < ((iNumber * 2) - 1)))
+            {
+                if (((iIterator2 == (iIterator1 - iNumber + 1))) || ((iIterator2 == (iNumber * 2) - (iIterator1 - iNumber + 1))))
+                {
+                    printf("*");
+                }
+                else
+                {
+                    printf(" ");
+                }
+            }
+            else
+            {
+                printf(" ");
+            }
+        }
+        printf("\n");
+    }
+}
